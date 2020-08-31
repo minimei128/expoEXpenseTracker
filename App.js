d7bdda9e718b48af60bbdc4555310c2ec89662a6
minimei128 <mphan2868@hotmail.com8>
2020-08-31 22:46:08 +1000

    added dependencies and firebase

--- App.js
+++ App.js
@@ -1,6 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer, StackActions } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+// firebase config
+import {firebaseConfig} from './config/firebase'
+// firebase library
+import * as firebase from 'firebase'
+// initialise app
+if ( !firebase.apps.length ){
+  firebase.initializeApp( firebaseConfig )
+}
 
 export default function App() {
   return (
---
+++ config/firebase.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyB8JgmAUQ7ur0CYcsu3f0zx27PTWH_aN74",
+    authDomain: "expotaskplanner.firebaseapp.com",
+    databaseURL: "https://expotaskplanner.firebaseio.com",
+    projectId: "expotaskplanner",
+    storageBucket: "expotaskplanner.appspot.com",
+    messagingSenderId: "859007043256",
+    appId: "1:859007043256:web:c7a498a1009cebe641d64d",
+    measurementId: "G-9W0HBVTDHM"
+  };
