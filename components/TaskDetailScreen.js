e5279b6c30aedf4c81ded3f7e3852deed9f9d74a
minimei128 <mphan2868@hotmail.com8>
2020-09-21 23:47:36 +1000

    added edit and save for each item

--- components/TaskDetailScreen.js
+++ components/TaskDetailScreen.js
@@ -1,11 +1,14 @@
 import React, {useState, useEffect} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import {DateFormat} from './DateFormat';
 
 //Task Detail Screen
 export const TaskDetailScreen = (props) => {
     const [amount, setAmount] = useState(props.route.params.amount)
     const [editing, setEditing] = useState(false)
+
+    const navigation = useNavigation()
 
     return (
         <View>
@@ -17,7 +20,26 @@ export const TaskDetailScreen = (props) => {
           style={[styles.amount, {display: editing ? 'flex' : 'none'}]} 
           placeholder={amount}
           onChangeText={(amount) => {setAmount(amount)}}/>
-          <Button title={editing? "Save" : "Edit"} onPress={ () => { editing ? setEditing(false) : setEditing}} />
+          
+          <Button 
+        title={ editing? "save" : "edit" } 
+        onPress={ () => { 
+          if( editing ) {
+            setEditing(false)
+            let item = {
+              amount: amount,
+              note: props.route.params.note,
+              category: props.route.params.category,
+              id: props.route.params.id
+            }
+            props.update( item )
+          }
+          else {
+            setEditing(true) 
+          }
+        } } 
+      />
+      
           <DateFormat 
           date={props.route.params.id} 
           styling={styles.date} />
